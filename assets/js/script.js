7dba8cd6357c85f961b6aa06dacace20b2447b0b
Jurgis <jurgis@Jurgiss-Air.lan>
2022-11-10 22:05:35 +0200

    nav-link_activate

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 		
-    window.addEventListener('scroll', function() {
-       
-        if (window.scrollY > 200) {
-            document.getElementById('navbar_top').classList.add('fixed-top');
-            // add padding top to show content behind navbar
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-             document.getElementById('navbar_top').classList.remove('fixed-top');
-             // remove padding top from body
-            document.body.style.paddingTop = '0';
-        } 
-    });
+  window.addEventListener('scroll', function() {
+     
+      if (window.scrollY > 200) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+           document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+      } 
+  });
 }); 
 
 
@@ -27,38 +27,43 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
-    mybutton.style.opacity = "1";
-    mybutton.style.transform = "translateY(0px)";
-    mybutton.style.transition = "0.3s";
-  } else {
-    mybutton.style.transform = "translateY(70px)";
-    mybutton.style.opacity = "0";
-   
-    
-  }
+if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  mybutton.style.display = "block";
+  mybutton.style.opacity = "1";
+  mybutton.style.transform = "translateY(0px)";
+  mybutton.style.transition = "0.3s";
+} else {
+  mybutton.style.transform = "translateY(70px)";
+  mybutton.style.opacity = "0";
+ 
+  
+}
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-  mybutton.style.transition = "transform 0.3s;";
-  
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+mybutton.style.transition = "transform 0.3s;";
+
 }
 
+// nav-link acive  //
 
-// hover//
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
 
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
 
-
-$(document).ready( function() {
-    // Get page URL by breaking down window.location
-    var page = window.location.href.substr(window.location.href.lastIndexOf("#sushi") + 1);
-
-    // Search through each link, compare href to current page
-    $("a.btncstm").each( function(){
-        if( $(this).attr("href") == page || $(this).attr("href") == "" )
-        $(this).addClass("active");
-    })
-});
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
