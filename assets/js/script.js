618b995b20193a6325765f90865a55a98ff8238c
Jurgis Bernatonis <jurgis.bernatonis@protonmail.com>
2022-11-14 23:32:02 +0200

    new-page

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,16 @@ window.onscroll = () => {
     }
   });
 };
+
+
+
+// galerija //
+document.addEventListener("click", function (e) {
+  if(e.target.classList.contains("gallery-item"))  {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+    myModal.show();
+  }
+});
+
